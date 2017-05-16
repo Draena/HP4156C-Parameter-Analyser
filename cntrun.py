32255bf4cb35435f6dbee5b20ebaf878a2bb843b
leobrowning92 <leobrowning92@gmail.com>
2017-05-17 08:53:47 +1200

    changed arguments for the series measurement

--- cntrun.py
+++ cntrun.py
@@ -4,8 +4,8 @@
 
 import parameter_analyser as pa
 import sys
-device = pa.initialize_device()
-def run_transfer_series(device, fname, savedir):
+def run_transfer_series(fname, savedir):
+    device = pa.initialize_device()
     pa.define_transfer_smu()
     pa.measure_transfer(device, fname, savedir, -10, 10, 0.1, 0.1, 0, 1)
     pa.measure_transfer(device, fname, savedir, -20, 20, 0.1, 0.1, 0, 1)
@@ -14,4 +14,4 @@ def run_transfer_series(device, fname, savedir):
     pa.measure_output(device, fname, savedir, -5, 5, 0.1, -20, 5, 9)
 
 if __name__ == "__main__":
-    run_transfer_series(device,"test_[INFO].csv","")
+    run_transfer_series("test_[INFO].csv","")
